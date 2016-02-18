2bf048a24b61281bc1ac14847687d5b24020c29a
Nathan <nathan.lea@okstate.edu>
2016-02-17 20:51:47 -0600

    Packet Creation Done

--- MercuryController/MainWindow.xaml.cs
+++ MercuryController/MainWindow.xaml.cs
@@ -25,7 +25,7 @@ namespace MercuryController
             Loaded += MainWindow_Loaded;
             Closing += MainWindow_Closing;
             InitializeComponent();
-            _timer = new DispatcherTimer { Interval = TimeSpan.FromMilliseconds(100) };
+            _timer = new DispatcherTimer { Interval = TimeSpan.FromMilliseconds(150) };
             _timer.Tick += _timer_Tick;
             _timer.Start();
         }
@@ -41,20 +41,73 @@ namespace MercuryController
             LeftAxis = string.Format("X: {0} Y: {1}", state.Gamepad.LeftThumbX, state.Gamepad.LeftThumbY);
             RightAxis = string.Format("X: {0} Y: {1}", state.Gamepad.RightThumbX, state.Gamepad.RightThumbY);
             ZAxis = string.Format("R: {0} L: {1}", state.Gamepad.RightTrigger, state.Gamepad.LeftTrigger);
-            //Buttons = string.Format("A: {0} B: {1} X: {2} Y: {3}", state.Gamepad.Buttons.ToString(), state.Gamepad.LeftThumbY);
             Buttons = string.Format("{0}", state.Gamepad.Buttons);
 
             xAxis.Content = LeftAxis;
             yAxis.Content = RightAxis;
             triggers.Content = ZAxis;
             buttonsLabel.Content = Buttons;
-            /*
-            Console.WriteLine(LeftAxis);
-            Console.WriteLine(RightAxis);
-            Console.WriteLine(Buttons);*/
+
+            // LY  -32768,32767
+            // LX  -32768,32767
+            // RX  -32768,32767
+            // RY  -32768,32767
+
+            // L R Trigger 0,255
+
+            //Right
+            double RightY = state.Gamepad.RightThumbY;
+            //Throttle
+            double throttle = ( ( RightY + 32768 ) * 0.00389099122 );
+            //throttle byte
+            byte thr = (Byte) throttle;
+
+            //Left
+            double LeftY = state.Gamepad.LeftThumbY;
+            //Steering
+            double steering = ( ( LeftY + 32768 ) * 0.00389099122 );
+            //throttle byte
+            byte steer = (Byte)steering;
+
+            //servo
+            byte[] servo = new byte[3];
+
+            //AUX?
+            byte[] aux = new byte[2];
+            int buttons = (int) state.Gamepad.Buttons;
+            Console.WriteLine(buttons);
+            aux[0] = (byte)( buttons & 0xFF );
+            aux[1] = (byte)((buttons & 0xFF00) >> 8);
+    
+            byte[] packet = new byte[10];
+
+            //Generate Noraml Packet
+            if( true )
+            {
+
+                packet[0] = 0x00; //mode byte
+                packet[1] = thr;
+                packet[2] = steer;
+                packet[3] = servo[0];
+                packet[4] = servo[1];
+                packet[5] = servo[2];
+                packet[6] = aux[0];
+                packet[7] = aux[1];
+
+                for(int i = 0; i < 8; i++)
+                {
+                    packet[8] += packet[i];
+                }
+
+                byte[] stuffedPacket = COBS.encode(packet);
+                packetLabelCOBS.Content = BitConverter.ToString(stuffedPacket);
+                packetLabel.Content = BitConverter.ToString(packet);
+
+            }
 
         }
 
+        
         void MainWindow_Closing(object sender, CancelEventArgs e)
         {
             _controller = null;
